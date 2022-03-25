1999cb65bbd685980747d3623f9aed0372d02bfa
Fizzify <21076@brightwoods.edu.ph>
2022-03-25 10:49:17 +0800

    Added animation and UI improvements.

--- app/js/index.js
+++ app/js/index.js
@@ -1,5 +1,17 @@
 var tl = gsap.timeline();
 
 tl.from(".hero-title", {
-    
-})
+  opacity: 0,
+  y: -50,
+  duration: 2,
+  ease: Power4.easeOut,
+});
+
+tl.from(".hero-btn", {
+    opacity: 0,
+    x: -50,
+    duration: 2,
+    ease: Power4.easeOut,
+    stagger: .2
+  }, "=-1");
+  
